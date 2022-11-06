afee3bc090b42407f508f5739a714caed9a989ce
PutraDarmawan20 <117572507+PutraDarmawan20@users.noreply.github.com>
2022-11-06 18:11:36 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtNPM = New System.Windows.Forms.TextBox()
+        Me.txtSemester = New System.Windows.Forms.TextBox()
+        Me.txtTugas = New System.Windows.Forms.TextBox()
+        Me.txtUTS = New System.Windows.Forms.TextBox()
+        Me.txtUAS = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtIPS = New System.Windows.Forms.TextBox()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtIPK = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(256, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(235, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "PENILAIAN MAHASISWA"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(56, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "NAMA MAHASISWA"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(56, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label3.Size = New System.Drawing.Size(34, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "NPM"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 145)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "SEMESTER"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(56, 182)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(75, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "NILAI TUGAS"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(58, 215)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(59, 15)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "NILAI UTS"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(56, 255)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(61, 15)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "NILAI UAS"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label8.Location = New System.Drawing.Point(56, 316)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(77, 15)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "TOTAL NILAI"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label9.Location = New System.Drawing.Point(240, 316)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(25, 15)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "IPS"
+        '
+        'txtNama
+        '
+        Me.txtNama.Location = New System.Drawing.Point(201, 68)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(100, 23)
+        Me.txtNama.TabIndex = 9
+        '
+        'txtNPM
+        '
+        Me.txtNPM.Location = New System.Drawing.Point(201, 103)
+        Me.txtNPM.Name = "txtNPM"
+        Me.txtNPM.Size = New System.Drawing.Size(100, 23)
+        Me.txtNPM.TabIndex = 10
+        '
+        'txtSemester
+        '
+        Me.txtSemester.Location = New System.Drawing.Point(201, 137)
+        Me.txtSemester.Name = "txtSemester"
+        Me.txtSemester.Size = New System.Drawing.Size(100, 23)
+        Me.txtSemester.TabIndex = 11
+        '
+        'txtTugas
+        '
+        Me.txtTugas.Location = New System.Drawing.Point(201, 174)
+        Me.txtTugas.Name = "txtTugas"
+        Me.txtTugas.Size = New System.Drawing.Size(100, 23)
+        Me.txtTugas.TabIndex = 12
+        '
+        'txtUTS
+        '
+        Me.txtUTS.Location = New System.Drawing.Point(201, 215)
+        Me.txtUTS.Name = "txtUTS"
+        Me.txtUTS.Size = New System.Drawing.Size(100, 23)
+        Me.txtUTS.TabIndex = 13
+        '
+        'txtUAS
+        '
+        Me.txtUAS.Location = New System.Drawing.Point(201, 255)
+        Me.txtUAS.Name = "txtUAS"
+        Me.txtUAS.Size = New System.Drawing.Size(100, 23)
+        Me.txtUAS.TabIndex = 14
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(44, 345)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(100, 23)
+        Me.txtTotal.TabIndex = 15
+        '
+        'txtIPS
+        '
+        Me.txtIPS.Location = New System.Drawing.Point(201, 345)
+        Me.txtIPS.Name = "txtIPS"
+        Me.txtIPS.Size = New System.Drawing.Size(100, 23)
+        Me.txtIPS.TabIndex = 16
+        '
+        'btnHitung
+        '
+        Me.btnHitung.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnHitung.Location = New System.Drawing.Point(117, 413)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(90, 37)
+        Me.btnHitung.TabIndex = 17
+        Me.btnHitung.Text = "HITUNG"
+        Me.btnHitung.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnHapus.Location = New System.Drawing.Point(285, 413)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(79, 37)
+        Me.btnHapus.TabIndex = 18
+        Me.btnHapus.Text = "HAPUS"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label10.Location = New System.Drawing.Point(365, 316)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(26, 15)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "IPK"
+        '
+        'txtIPK
+        '
+        Me.txtIPK.Location = New System.Drawing.Point(348, 345)
+        Me.txtIPK.Name = "txtIPK"
+        Me.txtIPK.Size = New System.Drawing.Size(100, 23)
+        Me.txtIPK.TabIndex = 20
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(656, 562)
+        Me.Controls.Add(Me.txtIPK)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.txtIPS)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtUAS)
+        Me.Controls.Add(Me.txtUTS)
+        Me.Controls.Add(Me.txtTugas)
+        Me.Controls.Add(Me.txtSemester)
+        Me.Controls.Add(Me.txtNPM)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents txtNPM As TextBox
+    Friend WithEvents txtSemester As TextBox
+    Friend WithEvents txtTugas As TextBox
+    Friend WithEvents txtUTS As TextBox
+    Friend WithEvents txtUAS As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents txtIPS As TextBox
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtIPK As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        txtNama.Text = Text()
+        txtNPM.Text = Text()
+        txtSemester.Text = Text()
+        txtTugas.Text = Text()
+        txtUTS.Text = Text()
+        txtUAS.Text = Text()
+        txtTotal.Text = Text()
+        txtIPS.Text = Text()
+        txtIPK.Text = Text()
+    End Sub
+
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+
+        Dim Nama = txtNama.Text()
+        Dim NPM = txtNPM.Text()
+        Dim Semester = txtSemester.Text()
+        Dim Tugas = txtTugas.Text()
+        Dim UTS = txtUTS.Text()
+        Dim UAS = txtUAS.Text()
+        Dim IPS = txtIPS.Text()
+        Dim IPK = txtIPK.Text()
+        Dim Totalnilai = txtTotal.Text()
+        txtTotal.Text = txtTugas.Text * 20 / 100 + txtUTS.Text * 30 / 100 + txtUAS.Text * 50 / 100
+        txtIPS.Text = txtTotal.Text * 4 / 100
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+End Class
